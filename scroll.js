ac57de3fd9224db4aceceb9dfeff15b81c0c41b9
Zux4e <Zux4e0228@gmail.com>
2024-10-07 09:25:35 +0700

    menambahkan

--- scroll.js
+++ scroll.js
@@ -9,18 +9,82 @@
 //     }
 // });
 
+// $(document).ready(function () {
+//     $('.navTrigger').click(function () {
+//         $(this).toggleClass('active');
+//         $('#mainListDiv').toggleClass('show');
+//     });
+    
+//     // Mengubah warna navbar saat scroll
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 50) {
+//             $('.navbar').css({
+//                 'background-color': 'rgba(0, 0, 0, 0.1)',
+//                 'backdrop-filter': 'blur(10px)' // Tetap mempertahankan backdrop-filter
+//             });
+//         } else {
+//             $('.navbar').css('background-color', 'transparent');
+//         }
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.navTrigger').click(function () {
+//         $(this).toggleClass('active');
+//         $('#mainListDiv').toggleClass('show');
+//     });
+
+//    
+//     $(window).scroll(function () {
+//         var scrollTop = $(this).scrollTop();
+//         var homeHeight = $('.home').outerHeight(); 
+//         var sliderHeight = $('.slider').outerHeight(); 
+//         var totalHeight = homeHeight + sliderHeight; 
+
+//         // Jika scroll berada di section slider
+//         if (scrollTop >= homeHeight && scrollTop < totalHeight) {
+//             $('.navbar').css({
+//                 'background-color': 'rgba(0, 0, 0, 0.1)',
+//                 'backdrop-filter': 'blur(10px)' 
+//             });
+//         }  else {
+//             
+//             $('.navbar').css({
+//                 'background-color': 'transparent',
+//                 'backdrop-filter': 'none'
+//             });
+//         }
+//     });
+// });
+
 $(document).ready(function () {
+    // Mengatur klik untuk toggle navTrigger
     $('.navTrigger').click(function () {
         $(this).toggleClass('active');
         $('#mainListDiv').toggleClass('show');
     });
-    
-    // Mengubah warna navbar saat scroll
+
+    // Mengubah gaya navbar saat scroll
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('.navbar').css('background-color', '#222');
+        var scrollTop = $(this).scrollTop();
+        var homeHeight = $('.home').outerHeight(); 
+        var dummyHeight = $('#dummy').outerHeight(); 
+        var sliderHeight = $('.slider').outerHeight(); 
+        var totalHeight = homeHeight + dummyHeight + sliderHeight; 
+
+       
+        if (scrollTop >= homeHeight + dummyHeight) {
+            $('.navbar').css({
+                'background-color': 'rgba(0, 0, 0, 0.1)',
+                'backdrop-filter': 'blur(10px)'
+            });
         } else {
-            $('.navbar').css('background-color', 'transparent');
+           
+            $('.navbar').css({
+                'background-color': 'transparent',
+                'backdrop-filter': 'none'
+            });
         }
     });
 });
+
