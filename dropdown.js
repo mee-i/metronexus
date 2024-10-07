020f509e4b70cd1b1af97722f0d83375e30026ad
unknown <nanda.ahmad2007@gmail.com>
2024-10-07 20:41:06 +0700

    Improvements

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,14 @@
-function toggleDropdown(event) {
+function toggleDropdown(event, elem) {
+    console.log(event);
+    console.log('Target: ', elem);
+    console.log('next', elem.nextElementSibling);
     event.preventDefault(); // Mencegah default action dari tautan
-    const dropdown = event.target.nextElementSibling; // Mengambil elemen dropdown berikutnya
-    const isVisible = dropdown.style.display === 'block'; // Memeriksa apakah dropdown sudah terlihat
+    const dropdown = elem.nextElementSibling; // Mengambil elemen dropdown berikutnya
+    const isVisible = dropdown.style.display == 'block'; // Memeriksa apakah dropdown sudah terlihat
+
+    // debugger;
+    console.log('isVisible', isVisible);
+    console.log('display', dropdown.style.display);
 
     // Menyembunyikan semua dropdown lain jika ingin hanya satu yang terbuka
     const allDropdowns = document.querySelectorAll('.dropdown');
@@ -15,7 +22,13 @@ function toggleDropdown(event) {
 
 // Menyembunyikan dropdown saat klik di luar
 window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-toggle')) {
+    console.log('Matches', event.target.matches('.dropdown-toggle'));
+    console.log(event.target);
+    if (
+        !event.target.matches('.dropdown-toggle') &&
+        !event.target.matches('.dropdown-toggle *')
+    ) {
+    // if (!event.target.classList.contains('dropdown-toggle')) {
         const allDropdowns = document.querySelectorAll('.dropdown');
         allDropdowns.forEach(dd => {
             dd.style.display = 'none'; // Sembunyikan semua dropdown
