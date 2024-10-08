ad71e2a501b814e8b2fb561c100e42ef717a0ad7
unknown <nanda.ahmad2007@gmail.com>
2024-10-08 23:05:55 +0700

    SLIDER!!!!

--- slide.js
+++ slide.js
@@ -1,33 +1,54 @@
 const tombols = [
-    document.querySelector('.tombol-uji-coba.kiri'),
-    document.querySelector('.tombol-uji-coba.kanan'),
+    document.querySelector('.slider-button.kiri'),
+    document.querySelector('.slider-button.kanan'),
 ];
 
 const elemenDiscroll = document.querySelector('.scroll-container');
+const sliderElements = elemenDiscroll.children[0].children;
+
+const slideIndicator = document.querySelector('.slide-pos');
 
 var scrollings = 0;
 
 console.log('tombols', tombols, 'elementDiscroll', elemenDiscroll, 'scrollings', scrollings);
 
-// function scrollLoad() {
-    console.log('Scroll load');
-    tombols[0].onclick = function() {
-        scrollings = Math.max(scrollings - 1, 0);
-        var element = elemenDiscroll.children[0].children[scrollings];
-        var x = element.getBoundingClientRect().left + elemenDiscroll.scrollLeft - 40;
-        elemenDiscroll.scroll({top: 0, left: x, behavior: 'smooth'});
-        console.log('tombols', tombols, 'elementDiscroll', elemenDiscroll, 'scrollings', scrollings, 'x', x);
-        // element?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+function setPosition() {
+    var element = sliderElements[scrollings];
+    var x = element.getBoundingClientRect().left + elemenDiscroll.scrollLeft - 40;
+    elemenDiscroll.scroll({top: 0, left: x, behavior: 'smooth'});
+    console.log('tombols', tombols, 'elementDiscroll', elemenDiscroll, 'scrollings', scrollings, 'x', x);
+    resetSliderIndicator();
+    slideIndicator.children[scrollings].style.width = '15px';
+}
+
+function initSlider() {
+    for (var i = 0; i < sliderElements.length; i++) {
+        var dot = document.createElement('div');
+        slideIndicator.appendChild(dot);
     }
-    
-    tombols[1].onclick = function() {
-        scrollings = Math.min(scrollings + 1, elemenDiscroll.children[0].children.length - 1);
-        var element = elemenDiscroll.children[0].children[scrollings];
-        var x = element.getBoundingClientRect().left + elemenDiscroll.scrollLeft - 40;
-        elemenDiscroll.scroll({top: 0, left: x, behavior: 'smooth'});
-        console.log('tombols', tombols, 'elementDiscroll', elemenDiscroll, 'scrollings', scrollings, 'x', x);
-        // element?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    resetSliderIndicator();
+}
+
+function resetSliderIndicator() {
+    for (var indicator of slideIndicator.children) {
+        indicator.style.width = '5px';
     }
-// }
+}
+
+initSlider();
+setPosition(scrollings);
+
+console.log('Scroll load');
+tombols[0].onclick = function() {
+    scrollings = Math.max(scrollings - 1, 0);
+    setPosition();
+}
+
+tombols[1].onclick = function() {
+    scrollings = Math.min(scrollings + 1, elemenDiscroll.children[0].children.length - 1);
+    setPosition();
+}
+
+
 
 // document.addEventListener('load', scrollLoad);
